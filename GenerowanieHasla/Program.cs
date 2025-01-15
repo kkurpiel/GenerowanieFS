e09ca711daf0cbe0cd9998d3b18546070db5a30e
Konrad Kurpiel <168281997+borubar3k@users.noreply.github.com>
2025-01-15 17:09:47 +0100

    Dodanie aplikacji szyfrującej hasło

---
+++ GenerowanieHasla/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        try 
+        { 
+            Console.WriteLine("Wprowadź swoje hasło\n");
+            var haslo = Console.ReadLine();
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes("HttpServiceKey1#");
+                aes.IV = Encoding.UTF8.GetBytes("##HttpServiceIV#");
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(haslo);
+                        }
+                        Console.WriteLine(Convert.ToBase64String(ms.ToArray()));
+                    }
+                }
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Wystąpił błąd podczas szyfrowania hasła");
+        }
+    }
+}
